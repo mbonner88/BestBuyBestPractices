e38acf3f01ab3314a61ecb679cc2d9d3d8d96068
Matthew Bonner <matthewsbonner@comcast.net>
2022-05-07 12:42:02 -0400

    init commit

---
+++ BestBuyBestPractices/DapperProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+
+namespace BestBuyBestPractices
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+        public DapperProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM Products;");
+        }
+
+        public void CreateProduct(string newName, double newPrice, int newCategoryID)
+        {
+            _connection.Execute("INSERT INTO Products (Name, Price, CategoryID) VALUES (@name, @price, @categoryID);",
+                new { name = newName, price = newPrice, categoryID = newCategoryID });
+        }        
+    }
+}
--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
 
 namespace BestBuyBestPractices
 {
@@ -6,7 +11,76 @@ namespace BestBuyBestPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            //Department implementation:
+            var deptRepo = new DapperDepartmentRepository(conn);
+            var departments = deptRepo.GetAllDepartments();
+
+            Console.WriteLine($"Printing all departments...");
+            foreach(var dept in departments)
+            {
+                Console.WriteLine($"ID:{dept.DepartmentID} Name:{dept.Name}");
+            }
+
+            Console.WriteLine($"Type new department name:");
+            var newDepartment = Console.ReadLine();
+            deptRepo.InsertDepartment(newDepartment);
+
+            Console.WriteLine($"Printing new department list...");
+            departments = deptRepo.GetAllDepartments();
+            foreach (var dept in departments)
+            {
+                Console.WriteLine($"ID:{dept.DepartmentID} Name:{dept.Name}");
+            }
+
+            //Product implementation:
+            var prodRepo = new DapperProductRepository(conn);
+            var products = prodRepo.GetAllProducts();
+
+            Console.WriteLine($"Printing all products...");
+            foreach(var prod in products)
+            {
+                Console.WriteLine($"ID:{prod.ProductID} Name:{prod.Name} Price:{prod.Price} CategoryID:{prod.CategoryID}");
+            }
+
+            var newProduct = new Product();
+
+            Console.WriteLine($"Type new product name:");
+            newProduct.Name = Console.ReadLine();
+
+            Console.WriteLine($"Type price(must be decimal) of {newProduct.Name}:");
+            var newProductPrice = Console.ReadLine();
+            while(!double.TryParse(newProductPrice, out double result))
+            {
+                Console.WriteLine($"Failed. Try again");
+                newProductPrice = Console.ReadLine();
+            }
+            newProduct.Price = double.Parse(newProductPrice);
+
+            Console.WriteLine($"Type CategoryID(must be integer) of {newProduct.Name}");
+            var newProductCategoryID = Console.ReadLine();
+            while(!int.TryParse(newProductCategoryID, out int result))
+            {
+                Console.WriteLine($"Failed. Try again");
+                newProductCategoryID = Console.ReadLine();
+            }
+            newProduct.CategoryID = int.Parse(newProductCategoryID);
+
+            prodRepo.CreateProduct(newProduct.Name, newProduct.Price, newProduct.CategoryID);
+
+            Console.WriteLine($"Printing new product list...");
+            products = prodRepo.GetAllProducts();
+            foreach(var prod in products)
+            {
+                Console.WriteLine($"ID:{prod.ProductID} Name:{prod.Name} Price:{prod.Price} CategoryID:{prod.CategoryID}");
+            }
         }
     }
 }
